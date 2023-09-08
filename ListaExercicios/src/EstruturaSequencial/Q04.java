b8194ed62de5f6caedab7dfae95986aa1cb92d23
Davi Slm <davisoaresdasilva1203@gmail.com>
2023-09-07 21:41:44 -0300

    Atividades

--- ListaExercicios/src/EstruturaSequencial/Q04.java
+++ ListaExercicios/src/EstruturaSequencial/Q04.java
@@ -1,7 +1,27 @@
+//Faça um Programa que peça as 4 notas bimestrais e mostre a média
 package EstruturaSequencial;
+import java.util.Scanner;
 
 public class Q04 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o valor da primeira nota: ");
+        double nota1 = input.nextDouble();
+
+        System.out.println("Digite o valor da segunda nota: ");
+        double nota2 = input.nextDouble();
+
+        System.out.println("Digite o valor da terceira nota: ");
+        double nota3 = input.nextDouble();
+
+        System.out.println("Digite o valor da quarta nota: ");
+        double nota4 = input.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        media = Math.round(media * 100.0) / 100.0;
+
+        System.out.println("A média das notas é: " + media);
 
     }
 }
